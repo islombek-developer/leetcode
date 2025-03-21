0402df10da68bff8e8d11074fb1e65f183d7f536
islombek-develober <islombekbotiraliyev1@gmail.com>
2025-03-21 20:30:40 +0500

    leetcode

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) :
-        
+    def twoSum(self, nums, target: int) :
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+
+        return []
+
+salom=Solution()
+d=salom.twoSum([2,7,11,15],9)
+print(d)
